5afd79bfbec92d039391e0570a802635ec22cd19
inhobaek-wj <inhobaek.wj@gmail.com>
2020-03-10 16:27:57 +0900

    change range

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const canvas = document.getElementById("js-canvas");
+const colors = document.getElementsByClassName("controls-color");
+const range = document.getElementById("js-range");
+const mode = document.getElementById("js-fill");
+
 canvas.width = 700;
 canvas.height = 700;
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
 
 // context makes us to manipulate pixels inside the canvas.
 const ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
-
-const colors = document.getElementsByClassName("controls-color");
-Array.from(colors).forEach(color => color.addEventListener("click", changeColor));
 
 let painting = false;
 
@@ -33,14 +47,12 @@ function onMouseMove(event) {
   }
 }
 
-if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
-}
-
-function changeColor(event) {
+function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
 }
+
+const handleRangeChange = (event) => {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+};
